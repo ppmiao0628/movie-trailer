37bee746985d2be49a1d312c7577047d032765fe
Keller Me <ppmiao0628@outlook.com>
2019-10-27 22:37:09 +0800

    int

--- server/index.js
+++ server/index.js
@@ -2,4 +2,10 @@
  * Created by kellerme on 2019/10/27
  */
 
-const Koa = require('koa')
+const Koa = require('koa2');
+const app = new Koa();
+
+app.use(async(ctx, next) = > {
+    ctx.body = '首页'
+})
+app.listen(4455)
