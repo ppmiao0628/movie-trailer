30e4417c88750514081b671610ecc0c1e6b68c65
Keller Me <ppmiao0628@outlook.com>
2019-10-29 06:18:06 +0800

    koa-views

--- server/index.js
+++ server/index.js
@@ -5,12 +5,14 @@
 const Koa = require('koa2');
 const app = new Koa();
 const {htmlTpl, ejsTpl, pugTpl} = require('./tpl/index.js');
-const ejs = require('ejs');
-const pug = require('pug');
+const views = require('koa-views');
+const {resolve} = require('path');
 
+app.use(views(resolve(__dirname, './views'),{extension:'pug'}));
 app.use(async(ctx, next) => {
-    ctx.type = 'text/html;charset=utf-8';
-    ctx.body = pug.render(pugTpl,{you:'ppm1',me:'keller1'});
+    // ctx.type = 'text/html;charset=utf-8';
+    // ctx.body = pug.render(pugTpl,);
+    await ctx.render('index',{you:'ppm1',me:'keller1'})
 });
 console.log('listen port 4455');
 app.listen(4455);
