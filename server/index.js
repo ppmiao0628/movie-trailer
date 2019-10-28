e9095fcc01e9b42931bbecc4de7415235c7787b9
Keller Me <ppmiao0628@outlook.com>
2019-10-28 21:08:15 +0800

    初始化静态页面

--- server/index.js
+++ server/index.js
@@ -4,8 +4,11 @@
 
 const Koa = require('koa2');
 const app = new Koa();
+const {normal} = require('./tpl/index.js');
 
-app.use(async(ctx, next) = > {
-    ctx.body = '首页'
-})
-app.listen(4455)
+app.use(async(ctx, next) => {
+    ctx.type = 'text/html;charset=utf-8';
+    ctx.body = normal;
+});
+console.log('listen port 4455');
+app.listen(4455);
---
+++ server/tpl/index.js
@@ -0,0 +1,7 @@
+/**
+ * Created by kellerme on 2019/10/28
+ */
+const normalTpl = require('./normal.js')
+module.exports = {
+    normal: normalTpl
+}
